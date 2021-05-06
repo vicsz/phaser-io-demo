73ba571af809d4506696d9dd5cb2dd5c344a2cb4
Victor Sz <vicsz@users.noreply.github.com>
2021-05-06 18:54:17 -0400

    Adding index.html

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(8081, function () {
   console.log('Example app listening on port 8081!')
