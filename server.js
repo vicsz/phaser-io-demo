868ecef1e19dca47e2eb1e2e76e60f830ecfde0a
Victor Sz <vicsz@users.noreply.github.com>
2021-05-06 19:05:16 -0400

    Serving Assets

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+app.use(express.static(__dirname + '/assets/'));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
