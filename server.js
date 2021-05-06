804da89f584de9aff69f307da555bad6463d16bb
Victor Sz <vicsz@users.noreply.github.com>
2021-05-06 19:21:00 -0400

    Sample Phaser.io game

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
-app.use(express.static(__dirname + '/assets/'));
+app.use('/assets', express.static('assets'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
